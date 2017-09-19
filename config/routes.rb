962e0c0165bfadf69bdc8d3722b05046c332cac9
keymaster777 <rileykeycleary@yahoo.com>
2017-09-19 12:12:41 -0700

    Fixed typo in portfolios

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   
-  get 'portfolio/:id', to: 'portfoliod#show', as: 'portfolio_show'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about',   to: "pages#about"
   get 'contact', to: "pages#contact"
