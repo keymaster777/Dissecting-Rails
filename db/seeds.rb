069bae9c3866444b5a95b15be0063815fbc20dcc
Riley <rileykeycleary@yahoo.com>
2017-09-19 23:38:16 +0000

    Integrated Technologies Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -77,3 +77,11 @@ end
         )
 end
 puts "9 portfolio items created"
+
+3.times do |technology|
+    Portfolio.last.technologies.create!(
+        name: "Technology #{technology}" 
+    )
+end
+
+puts "Three technologies created"
