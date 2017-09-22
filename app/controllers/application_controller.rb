2b1e7b00a2ae4869fd89d267f2a282cf27160ee7
Riley <rileykeycleary@yahoo.com>
2017-09-22 22:56:00 +0000

    Replaced current user code in applicationcontroller with concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
-  
-  def current_user
-    super || guest_user
-  end
-  
-  def guest_user
-    OpenStruct.new(name: "Guest User",
-                   first_name: "Guest",
-                   last_name: "User",
-                   email: "guest@example.com")
-  end
+  include CurrentUserConcern
 end
