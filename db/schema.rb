b8a497ca538745ef2c3f477f35f61e48e9c0f693
Riley <rileykeycleary@yahoo.com>
2017-09-26 21:07:42 +0000

    Added installation requirements for petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170921231924) do
+ActiveRecord::Schema.define(version: 20170926210424) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20170921231924) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
