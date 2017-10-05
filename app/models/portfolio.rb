b1c09ecee796326af08c7c61adbf6fa4c4c1e3ca
Riley <rileykeycleary@yahoo.com>
2017-10-05 18:18:22 +0000

    Updated portfolio to include attr for position

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -11,6 +11,10 @@ class Portfolio < ApplicationRecord
         where(subtitle: 'Angular')
     end
     
+    def self.by_position
+        order("position ASC")
+    end
+    
     def self.ruby_on_rails_portfolio_items
         where(subtitle: 'Ruby on Rails')
     end
